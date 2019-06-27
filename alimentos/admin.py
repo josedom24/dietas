b70f0271d3920a392d8257a2e56a0ea7823a8504
José Domingo Muñoz <josedom24@gmail.com>
2019-06-27 21:32:27 +0200

    model alimentos

--- alimentos/admin.py
+++ alimentos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Alimentos
+
+admin.site.register(Alimentos)
--- alimentos/models.py
+++ alimentos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Alimentos(models.Model):
+    nombre = models.CharField(default="",max_length=100)
+    proteinas = models.FloatField(verbose_name="Proteinas (gr)")
+    grasas = models.FloatField(verbose_name="Grasas (gr)")
+    carbohidratos = models.FloatField(verbose_name="Carbohidratos (gr)")
+    kilocalorias = models.FloatField(verbose_name="Kilocalorias (kcal)")
+    porcion_comestible = models.FloatField(verbose_name="Porción comestible (gr)")
+
+    class Meta:
+        verbose_name="Alimento"
+        verbose_name_plural="Alimentos"
